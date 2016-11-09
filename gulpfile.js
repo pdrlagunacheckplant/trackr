673be62cc1cba7ecedd35043f05e2bae3f3d9d99
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2016-11-09 00:31:13 -0200

    add dist folder to ignored files, and add normalize and some style

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
+const pug  = require('gulp-pug');
+const sass = require('gulp-sass');
 
-gulp.task('build', () => {
+gulp.task('styles', () => {
+  return gulp.src('app/renderer/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/dist/renderer/css/'));
+});
+
+gulp.task('build', ['styles'], () => {
   gulp.src('app/renderer/pug/*')
     .pipe(pug())
     .pipe(gulp.dest('app/dist'));
 });
+
+gulp.task('default', ['build']);
